389d2e544f4c4c895f39299f12383820fe2919f3
Илья Дорогокупец <stud1090352013@bntu.by>
2023-06-08 18:54:06 +0300

    08.07.2023

---
+++ src/main/java/by/dorogokupets/text/parser/LetterParser.java
@@ -0,0 +1,17 @@
+package by.dorogokupets.text.parser;
+
+import by.dorogokupets.text.composite.Composite;
+import by.dorogokupets.text.composite.Leaf;
+import by.dorogokupets.text.composite.Component;
+import by.dorogokupets.text.composite.TextType;
+
+public class LetterParser extends DataParser {
+    @Override
+    protected Component handleRequest(String text) {
+        if (text.length() == 1) {
+            return new Leaf(TextType.LETTER, text);
+        } else {
+            return new Composite(TextType.LETTER);
+        }
+    }
+}
